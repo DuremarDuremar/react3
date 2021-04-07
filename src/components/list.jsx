38b6145e47a742babdb8a400fc6ebec1f7bac4de
Ivan <duremar98@yandex.ru>
2021-04-07 19:59:06 +0300

    fest_log

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,25 +1,50 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { getAxiosFilm } from "../server/serverFest";
+
+const ListStyle = styled.section`
+  margin-top: 20px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 10px;
+`;
+
+const Item = styled.div`
+  img {
+    max-width: 100%;
+    height: 400px;
+  }
+`;
 
 const List = (props) => {
   const [items, setItems] = useState(null);
-  const [itemsView, setItemsView] = useState(0, 8);
+  const [itemsView, setItemsView] = useState([0, 8]);
   useEffect(() => {
     getAxiosFilm(props.fest, props.year, itemsView).then((response) =>
-      setItems(response)
+      setItems(!items ? response : [...items, ...response])
     );
-  }, [props.fest, props.year]);
+  }, [props.fest, props.year, itemsView]);
 
   console.log(items);
 
   if (items) {
     return (
-      <div>
-        {items.map((item, index) => {
-          return <div key={index}>{item.nameRu}</div>;
-        })}
-      </div>
+      <ListStyle>
+        <InfiniteScroll
+          dataLength={items.length}
+          next={() => setItemsView([items.length, items.length + 8])}
+          hasMore={true}
+        >
+          {items.map((item, index) => {
+            return (
+              <Item key={index}>
+                <img src={item.posterUrlPreview} alt="img" />
+              </Item>
+            );
+          })}
+        </InfiniteScroll>
+      </ListStyle>
     );
   } else {
     return <p>55</p>;
--- src/server/serverFest.js
+++ src/server/serverFest.js
@@ -11,42 +11,34 @@ export const getAxiosFilm = async (fest, year, itemsView) => {
       1048563,
       1048532,
       980327,
+      1070264,
+      1094629,
+      980367,
+      959484,
+      984309,
+      981957,
+      963346,
+      928796,
     ],
   };
 
   const listLink = `${fest + year}`;
 
-  // const res1 = await axios.get(
-  //   `https://kinopoiskapiunofficial.tech/api/v2.1/films/${array[listLink][0]}`,
-  //   {
-  //     method: "GET",
-  //     headers: {
-  //       "X-API-KEY": "3624a818-0f9b-4117-91dd-3f6624d9d171",
-  //     },
-  //   }
-  // );
-  // const res2 = await axios.get(
-  //   `https://kinopoiskapiunofficial.tech/api/v2.1/films/${array[listLink][1]}`,
-  //   {
-  //     method: "GET",
-  //     headers: {
-  //       "X-API-KEY": "3624a818-0f9b-4117-91dd-3f6624d9d171",
-  //     },
-  //   }
-  // );
+  const listRes = await array[listLink]
+    .slice(itemsView[0], itemsView[1])
+    .map((item) => {
+      const res = axios.get(
+        `https://kinopoiskapiunofficial.tech/api/v2.1/films/${item}`,
+        {
+          method: "GET",
+          headers: {
+            "X-API-KEY": "3624a818-0f9b-4117-91dd-3f6624d9d171",
+          },
+        }
+      );
 
-  const listRes = await array[listLink].slice(itemsView).map((item) => {
-    const res = axios.get(
-      `https://kinopoiskapiunofficial.tech/api/v2.1/films/${item}`,
-      {
-        method: "GET",
-        headers: {
-          "X-API-KEY": "3624a818-0f9b-4117-91dd-3f6624d9d171",
-        },
-      }
-    );
-    return res;
-  });
+      return res;
+    });
 
   return Promise.all(listRes)
     .then(function (values) {
